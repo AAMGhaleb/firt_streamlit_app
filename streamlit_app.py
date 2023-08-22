ab55614878da0eecddc426a9bcff886e877aa01f
Abdulrahman Ghaleb <89785940+AAMGhaleb@users.noreply.github.com>
2023-08-22 10:30:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,12 @@ password = snowflake_secrets["password"]
 # Use the connection information to interact with Snowflake
 # Example: Connect to Snowflake using your preferred library (e.g., snowflake-connector-python) 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
 
